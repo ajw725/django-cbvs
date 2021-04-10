d81c9b4699512c3da7552c7ccd1f713f0cc4cc3e
Andrew Weinstein <andrew@altvia.com>
2021-04-10 08:35:44 -0600

    injecting context with cbvs

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import View
-
-# Create your views here.
+from django.views.generic import TemplateView
 
 
-class HomePageViews(View):
-    def get(self, _request):
-        return HttpResponse('Class-based views are cool!')
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['index_content'] = 'This content was injected!'
+        return context
