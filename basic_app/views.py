188008acb28e4c39cba06d89dbdfe7d9f3155b6f
Andrew Weinstein <andrew@altvia.com>
2021-04-10 08:21:34 -0600

    Switch to basic class-based view

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'index.html')
+class HomePageViews(View):
+    def get(self, _request):
+        return HttpResponse('Class-based views are cool!')
