4ea296d4af9fc4c944d3c55198c482a415d19a01
Andrew Weinstein <andrew@altvia.com>
2021-04-10 10:35:06 -0600

    list and detail views

--- basic_app/models.py
+++ basic_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class School(BaseModel):
+    name = models.CharField(max_length=255)
+    principal = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(BaseModel):
+    name = models.CharField(max_length=255)
+    age = models.PositiveIntegerField()
+    school = models.ForeignKey(School, related_name='students', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- basic_app/views.py
+++ basic_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import School
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['index_content'] = 'This content was injected!'
-        return context
+
+class SchoolListView(ListView):
+    model = School
+    context_object_name = 'schools'
+
+
+class SchoolDetailView(DetailView):
+    model = School
+    template_name = 'basic_app/school_detail.html'
